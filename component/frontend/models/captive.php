a94f9ca9d0c07c01c12e6b6d4b60fc3273104465
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-02-06 21:51:04 +0200

    Allow some module positions to be rendered
    This is useful for things like headers, footers and custom HTML modules with 2FA information for the user.

--- component/frontend/models/captive.php
+++ component/frontend/models/captive.php
@@ -15,7 +15,7 @@ class LoginGuardModelCaptive extends JModelLegacy
 	 *
 	 * This is implemented with a trick. If you use jdoc tags to load modules the JDocumentRendererHtmlModules
 	 * uses JModuleHelper::getModules() to load the list of modules to render. This goes through JModuleHelper::load()
-	 * which triggers the onAfterCleanModuleList event after cleaning up the module list from duplicates. By resetting
+	 * which triggers the onAfterModuleList event after cleaning up the module list from duplicates. By resetting
 	 * the list to an empty array we force Joomla to not display any modules.
 	 *
 	 * Similar code paths are followed by any canonical code which tries to load modules. So even if your template does
@@ -23,9 +23,48 @@ class LoginGuardModelCaptive extends JModelLegacy
 	 */
 	public function killAllModules()
 	{
+		$allowedPositions = $this->getAllowedModulePositions();
+
 		$app = JFactory::getApplication();
-		$app->registerEvent('onAfterCleanModuleList', function (&$modules) {
-			$modules = array();
+		$app->registerEvent('onAfterModuleList', function (&$modules) use ($allowedPositions) {
+			if (empty($modules))
+			{
+				return;
+			}
+
+			if (empty($allowedPositions))
+			{
+				$modules = array();
+
+				return;
+			}
+
+			$filtered = array();
+
+			foreach ($modules as $module)
+			{
+				if (in_array($module->position, $allowedPositions))
+				{
+					$filtered[] = $module;
+				}
+			}
+
+			$modules = $filtered;
 		});
 	}
+
+	/**
+	 * Get a list of module positions we are allowed to display
+	 *
+	 * @return  array
+	 */
+	private function getAllowedModulePositions()
+	{
+		$res = array();
+
+		// TODO Load the list of allowed module positions from the component's settings
+
+		return $res;
+	}
+
 }
