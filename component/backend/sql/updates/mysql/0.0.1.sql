44a97f132485e93373cd0bb006ad6e35b9aa5104
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-02-11 16:29:38 +0200

    Assign a default TSV record
    Schema

--- component/backend/sql/updates/mysql/0.0.1.sql
+++ component/backend/sql/updates/mysql/0.0.1.sql
@@ -2,8 +2,9 @@
 CREATE TABLE `#__loginguard_tfa` (
   `id` SERIAL,
   `user_id` BIGINT UNSIGNED NOT NULL,
-  `title` varchar(180) NOT NULL,
-  `method` varchar(100) NOT NULL,
+  `title` VARCHAR(180) NOT NULL,
+  `method` VARCHAR(100) NOT NULL,
+  `default` TINYINT(1) NOT NULL DEFAULT 0,
   `options` MEDIUMTEXT null,
   INDEX idx_user_id (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
