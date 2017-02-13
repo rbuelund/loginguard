0d812aacc6749eeb99350d27e411b6df0180dcee
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-02-13 19:50:25 +0200

    Front-end menus

--- component/frontend/loginguard.php
+++ component/frontend/loginguard.php
@@ -8,6 +8,27 @@
 // Prevent direct access
 defined('_JEXEC') or die;
 
+// I have to do some special handling to accommodate for the discrepancies between how Joomla creates menu items and how
+// Joomla handles component controllers. Ugh!
+$app = JFactory::getApplication();
+$view = $app->input->getCmd('view');
+$task = $app->input->getCmd('task');
+
+if (!empty($view))
+{
+	if (strpos($task, '.') === false)
+	{
+		$task = $view . '.' . $task;
+	}
+	else
+	{
+		list($view, $task2) = explode('.', $task, 2);
+	}
+
+	$app->input->set('view', $view);
+	$app->input->set('task', $task);
+}
+
 // Get an instance of the LoginGuard controller
 $controller = JControllerLegacy::getInstance('LoginGuard');
 
