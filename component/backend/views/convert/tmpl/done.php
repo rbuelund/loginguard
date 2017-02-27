3a0015cb3a43429d55cd5dec82083eb0d7c190f5
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-02-27 20:08:39 +0200

    Close gh-4 Migration from TFA plugins
    Migrate from YubiKey (Joomla), TOTP (Joomla), YubiKey + TOTP (Akeeba)

---
+++ component/backend/views/convert/tmpl/done.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package   AkeebaLoginGuard
+ * @copyright Copyright (c)2016-2017 Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+// Prevent direct access
+defined('_JEXEC') or die;
+
+/** @var LoginGuardViewConvert $this */
+
+?>
+<h1><?php echo JText::_('COM_LOGINGUARD_CONVERT_DONE_HEAD'); ?></h1>
+
+<p>
+	<?php echo JText::_('COM_LOGINGUARD_CONVERT_DONE_INFO'); ?>
+</p>
