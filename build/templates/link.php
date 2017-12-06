8fb214b56057279861c4a74a5b5313a877a336b0
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-12-06 15:40:13 +0200

    Rewrite to use FOF and FEF
    Update internal linker

--- build/templates/link.php
+++ build/templates/link.php
@@ -3,8 +3,14 @@ $hardlink_files = array(
 );
 
 $symlink_files = array(
+	// FEF styling
+	'../fef/j-css/style.min.css' => 'component/media/css/fef.min.css',
 );
 
 $symlink_folders = array(
 	'../translations/loginguard' => 'translations',
+
+	// FEF Fonts and images
+	'../fef/fonts'           => 'component/media/fonts',
+	'../fef/images'          => 'component/media/images',
 );
