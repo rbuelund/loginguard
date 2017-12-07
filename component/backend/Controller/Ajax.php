f736fa29d04f449157b7727676ed09ef129eb8ec
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-12-07 14:48:00 +0200

    Rewrite to use FOF and FEF
    Finish the backend

---
+++ component/backend/Controller/Ajax.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package   AkeebaLoginGuard
+ * @copyright Copyright (c)2016-2017 Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\LoginGuard\Admin\Controller;
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+class Ajax extends Akeeba\LoginGuard\Site\Controller\Ajax
+{
+
+}
